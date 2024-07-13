b54496ff335cba7130d1edd13c03c615078cc6bd
muneeb786-pixel <muneeburr451@gmail.com>
2024-07-13 17:30:07 +0500

    :sparkles: Completed Challenge 1 Variable and data type

---
+++ day-01-variables.js
@@ -0,0 +1,47 @@
+// Activity 1 Variable declaration.
+// Task 1: Declare a variable using var, assign it a number and  log to console
+
+var number = 349;
+console.log(number);
+
+//  Task 2: Declare a variable using let, assign it a string and log to console.
+
+let string = "Hello Muneeb";
+console.log(string);
+
+// Activity 2 Constant Declaration
+//  Task 1: Declare a variable with const, assign it a boolean value, and log to console.
+const IS_LOGIN = true;
+console.log(IS_LOGIN);
+
+// Activity 3 Data Type
+// Task 1: Create a variable of different data type ( number,string,boolean,array and object ), and log each variable type using typeof
+let numberVar = 10;
+let stringVar = "Muneeb ur rehman";
+let booleanVar = true;
+let arrayList = ["cool", 707, "bravo", true];
+let myAddress = {
+  address1: "House # 212, karam shah wali, mailsi,vehari",
+  address2: "",
+  Country: "Pakistan",
+  Province: "Punjab",
+  City: "Mailsi",
+};
+console.log(typeof numberVar);
+console.log(typeof stringVar);
+console.log(typeof booleanVar);
+console.log(typeof arrayList);
+console.log(typeof myAddress);
+
+// Activity 4 Reassigning value
+// Declare a variable with let and assign initial value, reassign a new value, and log both values.
+let phone = "03138525589";
+console.log(phone);
+phone = "03138525589";
+console.log(phone);
+
+// Activity 5 Understand const
+// try to assign value to constant variable.
+
+const CNIC = "3660276482933";
+CNIC = "3668299876500";
